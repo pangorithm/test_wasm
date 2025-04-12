5c77e21b56135277c0ca0a5d8673411c545d5d67
pangorithm <phs559800@gmail.com>
2025-04-12 15:07:03 +0900

    빌드 시 경로 설정 변경

--- test_wasm_front/src/App.jsx
+++ test_wasm_front/src/App.jsx
@@ -1,12 +1,18 @@
-import { Router, Route, A } from "@solidjs/router";
+import { Router, Route } from "@solidjs/router";
+import { onMount } from "solid-js";
 import Home from "./pages/Home";
 import Fibonacci from "./pages/Fibonacci";
 import Sql from "./pages/Sql";
 
 export default function App() {
+  const base = import.meta.env.BASE_URL;
+  onMount(() => {
+    console.log("✅ JS 실행됨");
+  });
+
   return (
     <div>
-      <Router>
+      <Router base={base}>
         <Route path="/" component={Home} />
         <Route path="/fibonacci" component={Fibonacci} />
         <Route path="/sql" component={Sql} />
