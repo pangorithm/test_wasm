8f3945cef419590d02c2cbde6558da9e68e26ec6
pangorithm <phs559800@gmail.com>
2025-04-12 14:43:27 +0900

    빌드 옵션 변경

--- test_wasm_front/vite.config.js
+++ test_wasm_front/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "/test_wasm/", // GitHub Pages 배포 경로에 맞게 설정
   plugins: [solid()],
 });
